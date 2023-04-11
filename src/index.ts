ef7fd15b2738381fdc55481afe278eb2988784a6
Austin Wang <yyaustin@bu.edu>
2023-04-11 05:00:37 -0400

    Finish exercise 9.10

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,19 @@
 import express from "express"
+import cors from "cors"
+import diagnoseRouter from "./routes/diagnoses"
 const app = express()
 
 app.use(express.json())
+app.use(cors())
 
 app.get("/api/ping", (_req, res) => {
   console.log("someone pinged here")
   res.send("pong")
 })
 
-const PORT = 3002
+app.use("/api/diagnoses", diagnoseRouter)
+
+const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
